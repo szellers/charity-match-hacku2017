43ff90e322cef40cf813018c8db67b9a909dff0e
westonjackson <weston.j.jackson@gmail.com>
2017-07-27 15:34:41 -0400

    server api call

--- web/src/components/MainPageNavBar.js
+++ web/src/components/MainPageNavBar.js
@@ -3,27 +3,75 @@ import React, { Component } from 'react';
 import Button from './Button';
 import FilterBox from './FilterBox';
 import TextInput from './TextInput';
+import $ from 'jquery';
 
 export default class MainPageNavBar extends Component {
 	constructor(props) {
 		super(props);
-
-		this.state = {
-			filters: []
-		};
+    
+        this.dictionary = {
+                "Advocacy & Human Rights":           "1", 
+                "Animals":             "2", 
+                "Arts & Culture":               "3", 
+                "Children & Youth":                 "4", 
+                "Computers & Technology":                   "5", 
+                "Crisis Support":                     "6", 
+                "Education & Literacy":                       "7", 
+                "Emergency & Safety":                         "8", 
+                "Environment":                           "9", 
+                "Homeless & Housing":                             "10", 
+                "Immigrants & Refugees":                               "11", 
+                "LGBTQ+":                                 "12", 
+                "People with Disabilities":                                   "13", 
+                "Politics":                                     "14", 
+                "Race & Ethnicity":                                       "15", 
+                "Seniors":                                         "16", 
+                "Sports & Recreation":                                           "17", 
+                "Veterans & Military Families":                                             "18", 
+                "Women": "19"
+        };
+    	
+		this.data = {};
+        this.state = {value: "1"};
 	}
 
 	_onFilterEnter = (text) => {
 		this.state.filters.push(text);
 
 		this.setState({
-			filters: this.state.filters
+	        filters: this.state.filters
 		});
 	}
 
 	_createFilterBox = (filter) => {
 		return <FilterBox name={filter} />;
 	}
+
+    _createOption = (option) => {
+        return <option value={this.dictionary[option]}> {option} </option>;
+    }
+
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state.value);
+        $.ajax({
+      		url: 'http://charity-matcher.devnxs.net:1234/get_events_for_category?category_id=' + this.state.value,
+			type: 'GET',
+      		cache: false,
+      		success: function(data) {        		
+				this.setState({data: data});
+                console.log(this.data);
+      		}.bind(this),
+      		error: function(xhr, status, err) {
+        		console.log(this.props.url, status, err.toString());
+      		}.bind(this)
+    	});     
+         
+    }
 
 	render() {
 		const { filters } = this.state;
@@ -33,12 +81,12 @@ export default class MainPageNavBar extends Component {
 			<div id="search-bar-container">
 			    <form>
                     <TextInput id="search" className="common-text-input" name="search" placeholder="Search" />
-					<TextInput id="filter" className="common-text-input" name="filter" placeholder="Filter" onEnter={this._onFilterEnter} />
-					<div id="filters">
-						{filters && filters.map(this._createFilterBox)}
-					</div>
-				
-                    <Button className="common-btn" id="search-button">Submit</Button>
+			        
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        {Object.keys(this.dictionary).map(this._createOption)} 
+                    </select>
+            	
+                    <Button className="common-btn" id="search-button" onClick={this.handleSubmit}>Submit</Button>
                 </form>
 			</div>
 		);
