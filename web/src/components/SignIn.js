370034feee0d553b967528572f4d6bba63b01dee
Aadesh Patel <apatel@452.bm-adserving.user.nym2>
2017-07-27 18:19:30 +0000

    added router

--- web/src/components/SignIn.js
+++ web/src/components/SignIn.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 import Button from './Button';
 import TextInput from './TextInput';
 
 export default class SignIn extends Component {
+	_onSignIn = () => {
+		browserHistory.push('/main');
+	}
+
 	render() {
 		return (
 			<div id="sign-in">
@@ -15,7 +20,7 @@ export default class SignIn extends Component {
 						placeholder="AppNexus ID"
 						name="id" />
 
-					<Button className="common-btn" id="sign-in__sign-in-btn">Sign In</Button>
+					<Button className="common-btn" id="sign-in__sign-in-btn" onClick={this._onSignIn}>Sign In</Button>
 				</form>
 			</div>
 		);
--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Routes from './routes';
+
+ReactDOM.render(
+	<Routes history={browserHistory} />,
+	document.getElementById('root')
+);
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
