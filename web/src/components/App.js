d24a2b2197dd2b00587ac5547613a6f6aa6807b1
Aadesh Patel <apatel@452.bm-adserving.user.nym2>
2017-07-27 14:54:02 +0000

    initial web commit

---
+++ web/src/components/App.js
@@ -0,0 +1,15 @@
+import '../stylesheets/main.scss';
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div id="app">
+				<div id="outer-container">
+					HELLO
+				</div>
+			</div>
+		);
+	}
+}
